084a9bc36d29d3c92f77a2dfe5e2558246c7b97d
Shaked <shakedlevi32@gmail.com>
2022-01-30 16:34:23 +0200

    gui integration with controller

--- Client/Controller/Controller.py
+++ Client/Controller/Controller.py
@@ -1,31 +1,44 @@
-import os
-import sys
-from tkinter import Text, END
+from tkinter import Text, END, DISABLED, NORMAL, Entry, Tk, Toplevel
 
 from Client.backend.template_client import Client
 
 
 class Controller:
+    """
+    This class is the brain for our Gui, it will use the server and display the required data on the screen,
+    as requested by the client.
+    """
 
     def __init__(self, addr):
-        self.client = Client()
-        self.client.connect(addr)
+        # self.client = Client()
+        # self.client.connect(addr)
+        pass
 
-    def disconnect(self):
+    def exit_chat(self, login: Toplevel, chat: Tk):
         """
         This method disconnect a Client from the chat, and returns him to the menu
         :return:
         """
-        self.client.disconnect()
+        # self.client.disconnect()
+        login.deiconify()
+        chat.withdraw()  # TODO: fix this to make the chat "disappear" and to not show old contents after reestablishing connection
 
-    # TODO: sending messages should integrate with the msg_box and only allow to send when clicked on it
-    def send_msg(self, msg: str, text_box: Text):
+    @staticmethod
+    def send_msg(text_box: Text, msg_box: Entry):
         """
         This method displays a message to certain person in the chat
         :return:
         """
         # self.client.send(msg)
+        # Extract data from client:
+        msg = msg_box.get()
+        if msg == "":
+            return
+        msg_box.delete(0, END)
+        # Display the msg:
+        text_box.config(state=NORMAL)
         text_box.insert(END, '\nME: ' + msg)
+        text_box.config(state=DISABLED)
         text_box.update()
 
     def send_all(self):
@@ -49,19 +62,11 @@ class Controller:
         """
         pass
 
-    def clear_chat(self):
+    def clear_chat(self, text_box: Text):
         """
         This method removes all the data from the chat.
         :return:
         """
-        pass
-
-    def resource_path(relative_path):
-        """ Get absolute path to resource, works for dev and for PyInstaller """
-        try:
-            # PyInstaller creates a temp folder and stores path in _MEIPASS
-            base_path = sys._MEIPASS
-        except Exception:
-            base_path = os.path.abspath("")
-
-        return os.path.join(base_path, relative_path)
+        text_box.config(state=NORMAL)
+        text_box.delete('1.0', END)
+        text_box.config(state=DISABLED)
--- Client/frontend/client_gui.py
+++ Client/frontend/client_gui.py
@@ -2,27 +2,37 @@ from tkinter import *
 from PIL import ImageTk, Image
 from Client.Controller.Controller import Controller
 import os
+from Utilities import Misc
 
 
-class Room():
+class Room:
+    """
+    This clas is responsible for creating the Gui for our chat.
+    in particular - LOGIN window & Chat room window
+    """
 
     def __init__(self):
         # This is the chat window, it will be hidden for new users until they join the room.
         self.chat_window = Tk()
+        self.controller = Controller(("127.0.0.1", 12345))
         # imgs abs paths:
         os.chdir(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
-        parent_path = Controller.resource_path(relative_path='frontend')
-        self.images_path = parent_path + "\\imgs\\"
+        parent_path = Misc.resource_path(relative_path='frontend')
+        self.images_path = parent_path + "/imgs/"
         self.chat_window_builder()
         self.chat_window.withdraw()
-
         # login window
         self.chat_login = Toplevel()
+        self.chat_login.protocol("WM_DELETE_WINDOW", self.chat_window.destroy)  # terminates program
         self.chat_login_builder()
         # self.enter_chat()
         self.chat_window.mainloop()
 
     def chat_login_builder(self):
+        """
+        This method creates the login window
+        :return:
+        """
         self.chat_login.title("Login")
         self.chat_login.resizable(width=FALSE, height=FALSE)
         self.chat_login.configure(width=400, height=300)
@@ -36,91 +46,81 @@ class Room():
         connect.place(relx=0.4, rely=0.55)
 
     def chat_window_builder(self):
+        """
+        This method creates the chat window, basic gui usage - buttons,labels,text boxes..
+        :return:
+        """
         self.chat_window.title("Walk2Talk")
         self.chat_window.resizable(width=TRUE, height=TRUE)
         self.chat_window.configure(width=700, height=600)
         # create background:
         self.generate_background()
 
-        # Server msg box:
-        self.creator(operator="Text", text="", command=0.974, relheight=0.6530,
-                     relwidth=0.6980, relx=0.0080,
-                     rely=0.19)
+        # chat_box:
+        chat_box = Text(self.chat_window, font=("Helvetica", 14))
+        chat_box.config(state=DISABLED)
+        chat_box.place(relheight=0.6530, relwidth=0.6980, relx=0.0080, rely=0.19)
+        self.scrollbar(0.972, chat_box)
 
         # Data BOX:
-        self.creator(operator="Text", text="", command=0.92, relheight=0.6530,
-                     relwidth=0.2680, relx=0.723,
-                     rely=0.19)
+        data_box = Text(self.chat_window, font=("Helvetica", 14))
+        data_box.config(state=DISABLED)
+        data_box.place(relheight=0.6530, relwidth=0.2680, relx=0.723, rely=0.19)
+        self.scrollbar(0.92, data_box)
 
         # Client msg box:
-        self.creator(operator="Entry", text="", command=None, relheight=0.1050,
-                     relwidth=0.6975, relx=0.01,
-                     rely=0.8875)
+        client_msg = Entry(self.chat_window)
+        client_msg.place(relheight=0.1050, relwidth=0.6975, relx=0.01, rely=0.8875)
 
         # disconnect button:
-        self.creator(operator="Button", text="Exit Chat", command=Controller.disconnect, relheight=0.0570,
-                     relwidth=0.1190, relx=0.0080,
-                     rely=0.0075)
+        exit_chat = Button(self.chat_window, text="Exit Chat",
+                           command=lambda: self.controller.exit_chat(self.chat_login, self.chat_window))
+        exit_chat.place(relheight=0.0570, relwidth=0.1190, relx=0.0080, rely=0.0075)
 
         # send msg button:
-        self.creator(operator="Button", text="Send", command=Controller.send_msg, relheight=0.1050,
-                     relwidth=0.135, relx=0.7180,
-                     rely=0.8875)
+        send_msg = Button(self.chat_window, text="Send",
+                          command=lambda: self.controller.send_msg(text_box=chat_box, msg_box=client_msg))
+        send_msg.place(relheight=0.1050, relwidth=0.135, relx=0.7180, rely=0.8875)
 
         # send all button:
-        self.creator(operator="Button", text="Send All", command=Controller.send_all, relheight=0.1050,
-                     relwidth=0.135, relx=0.8580,
-                     rely=0.8875)
+        send_msg = Button(self.chat_window, text="Send All", command=self.controller.send_all)
+        send_msg.place(relheight=0.1050, relwidth=0.135, relx=0.8580, rely=0.8875)
 
         # get clients button:
-        self.creator(operator="Button", text="Show Connected", command=Controller.get_clients, relheight=0.0570,
-                     relwidth=0.2680, relx=0.7250,
-                     rely=0.0075)
+        get_clients = Button(self.chat_window, text="Show Connected",
+                             command=self.controller.get_clients)
+        get_clients.place(relheight=0.0570, relwidth=0.2680, relx=0.7250, rely=0.0075)
 
         # files / clients label:
-        self.creator(operator="Label", text="Files/Connected Clients", command=None, relheight=0.0650,
-                     relwidth=0.27, relx=0.723,
-                     rely=0.0750)
+        data = Label(self.chat_window, text="Files/Connected Clients")
+        data.place(relheight=0.0650, relwidth=0.27, relx=0.723, rely=0.0750)
 
         # get files button
-        self.creator(operator="Button", text="Show Files", command=Controller.get_files, relheight=0.0570,
-                     relwidth=0.2680, relx=0.442,
-                     rely=0.0075)
+        get_files = Button(self.chat_window, text="Show Files", command=self.controller.get_files)
+        get_files.place(relheight=0.0570, relwidth=0.2680, relx=0.442, rely=0.0075)
 
         # clear chat button:
-        self.creator(operator="Button", text="Clear Chat", command=Controller.clear_chat, relheight=0.0650,
-                     relwidth=0.27, relx=0.442,
-                     rely=0.0750)
+        clear_chat = Button(self.chat_window, text="Clear Chat",
+                            command=lambda: self.controller.clear_chat(text_box=chat_box))
+        clear_chat.place(relheight=0.0650, relwidth=0.27, relx=0.442, rely=0.0750)
 
-    def creator(self, operator: str, text: str, command, relheight: float, relwidth: float, relx: float, rely: float):
+    def scrollbar(self, x: float, txt: Text):
         """
-        This method is a creator for the objects we present in the window
-        :param operator: operator to tell the method which object to create
-        :param text: Name of the object
-        :param command: a method to operate on the button, on Text its used as a scrollbar y position
-        :param relheight: float representing height of object
-        :param relwidth: float representing width of object
-        :param relx: float representing x pos of the object
-        :param rely: float representing y pos of the object
-        :return: None
+        This method creates the scroll bar for the chat boxes
+        :param x: representing float number for locating the bar
+        :param txt: a text box to place the scroll bar on
+        :return:
         """
-        parts = None
-        if operator == 'Button':
-            parts = Button(self.chat_window, text=text, command=command)
-        if operator == 'Label':
-            parts = Label(self.chat_window, text=text)
-        if operator == 'Entry':
-            parts = Entry(self.chat_window)
-        if operator == 'Text':
-            parts = Text(self.chat_window, font=("Helvetica", 32))
-            parts.config(state=DISABLED)
-            # scrollbar:
-            scrollbar = Scrollbar(parts)
-            scrollbar.place(relheight=1, relx=command)
-            scrollbar.config(command=parts.yview)
-        parts.place(relheight=relheight, relwidth=relwidth, relx=relx, rely=rely)
+        scrollbar = Scrollbar(txt)
+        scrollbar.place(relheight=1, relx=x)
+        scrollbar.config(command=txt.yview)
 
     def generate_background(self):
+        # TODO: make this resizable
+        """
+        This method generates the background for our chat box,
+        :return:
+        """
         # Background load:
         template = Image.open(self.images_path + "Template.png")
         img = ImageTk.PhotoImage(template)
@@ -129,47 +129,9 @@ class Room():
         bg.pack(side='top', fill='both', expand='yes')
 
     def enter_chat(self):
-        self.chat_login.destroy()
+        self.chat_login.withdraw()
         self.chat_window.deiconify()  # TODO: MOVE TO CONTROLLER
 
 
 if __name__ == '__main__':
     Room()
-"""
-THIS IS OLD CREATOR: 
-
- # Client msg box:
-        client_msg = Entry(self.chat_window)
-        client_msg.place(relheight=0.1050, relwidth=0.6975, relx=0.01, rely=0.8875)  # MEASURES FOR THE Client msg box
-
-        # disconnect button:
-        # ELAD -> i want this command to make the Client to return to the login menu again.
-        exit_chat = Button(self.chat_window, text="Exit Chat",
-                           command=Controller.disconnect)
-        exit_chat.place(relheight=0.0570, relwidth=0.1190, relx=0.0080, rely=0.0075)
-
-        # send msg button:
-        send_msg = Button(self.chat_window, text="Send", command=Controller.send_msg)
-        send_msg.place(relheight=0.1050, relwidth=0.135, relx=0.7180, rely=0.8875)
-
-        # send all button:
-        send_msg = Button(self.chat_window, text="Send All", command=Controller.send_all)
-        send_msg.place(relheight=0.1050, relwidth=0.135, relx=0.8580, rely=0.8875)
-
-        # get clients button:
-        get_clients = Button(self.chat_window, text="Show Connected",
-                             command=Controller.get_clients)
-        get_clients.place(relheight=0.0570, relwidth=0.2680, relx=0.7250, rely=0.0075)
-
-        # files / clients label:
-        data = Label(self.chat_window, text="Files/Connected Clients")
-        data.place(relheight=0.0650, relwidth=0.27, relx=0.723, rely=0.0750)
-
-        # get files button
-        get_files = Button(self.chat_window, text="Show Files", command=Controller.get_files)
-        get_files.place(relheight=0.0570, relwidth=0.2680, relx=0.442, rely=0.0075)
-
-        # clear chat button:
-        clear_chat = Button(self.chat_window, text="Clear Chat", command=Controller.clear_chat)
-        clear_chat.place(relheight=0.0650, relwidth=0.27, relx=0.442, rely=0.0750)
-"""
